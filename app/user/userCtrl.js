e2ff4a3dae5c438062047f4dd67ac3bd354df973
glickid <glickid@yahoo.com>
2018-07-31 11:23:35 +0300

    reset user info from scope on login/logout/error

--- app/user/userCtrl.js
+++ app/user/userCtrl.js
@@ -1,18 +1,23 @@
 app.controller('userCtrl', function ($scope, $log, $location, userSrv) {
     $scope.user = userSrv.getActiveUser();
 
+    resetScopeInfo();
+
+    function resetScopeInfo ()
+    {
     $scope.email = "";
     $scope.password = "";
     $scope.fname = "";
-    $scope.lname = ""
-
+    $scope.lname = "";
     $scope.invalidLogin = false;
     $scope.errorMessage = "";
+    }
 
     $scope.signup = function () {
         var newUser = { "fname": $scope.fname, "lname": $scope.lname, "email": $scope.email, "password": $scope.password }
         userSrv.createNewUser(newUser);
         $location.path("#!/");
+        resetScopeInfo();
     }
 
     $scope.login = function () {
@@ -20,6 +25,7 @@ app.controller('userCtrl', function ($scope, $log, $location, userSrv) {
         userSrv.login($scope.email, $scope.password).then(function (activeUser) {
             $location.path("#!/");
             $('#SignInModal').modal('hide');
+            resetScopeInfo();
         }, function () {
             $scope.invalidLogin = true;
             $scope.errorMessage = "Invalid Username or password!";
@@ -27,10 +33,7 @@ app.controller('userCtrl', function ($scope, $log, $location, userSrv) {
     }
 
     $scope.resetModal = function () {
-        $scope.email = "";
-        $scope.password = "";
-        $scope.errorMessage = "";
-        $scope.invalidLogin = false;
+        resetScopeInfo();
         // $('#SignInModal').modal('hide');
     }
 })
