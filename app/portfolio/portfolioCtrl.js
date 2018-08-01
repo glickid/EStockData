b12bd1ec95278490654223b2579397e562739794
glickid <glickid@yahoo.com>
2018-08-01 17:01:10 +0300

    Add refresh stock logic

--- app/portfolio/portfolioCtrl.js
+++ app/portfolio/portfolioCtrl.js
@@ -30,48 +30,70 @@ app.controller('portfolioCtrl', function ($scope, $location, dataSrv, configSrv,
                         });
                     }
                 }, function (err) {
-                console.log(err);
-            });
+                    console.log(err);
+                });
 
-    }, function (err) {
-        console.log(err);
-    })
-    }
-
-$scope.searchStock = function (searchStr) {
-    if (searchStr.length > 1) {
-        dataSrv.searchStock(searchStr).then(function (response) {
-            $scope.stockList = response;
         }, function (err) {
             console.log(err);
-            $scope.stockList = "";
         })
     }
-}
 
-$scope.addStockToPortfolio = function (stockName, stockSymbol) {
+    $scope.refreshStock = function (stock) {
+        dataSrv.getStockInfo(stock["Name"], stock["Symbol"]).then(function (response) {
+            portfolioSrv.updateStockInPortfolio(stock["Name"], stock["Symbol"], response)
+                .then(function (response1) {
 
-    dataSrv.getStockInfo(stockName, stockSymbol).then(function (response) {
-        portfolioSrv.addStockToPortfolio(stockName, stockSymbol, response).then(function (response1) {
-            $scope.stockArr.push({
-                "Name": response1.name, "Symbol": response1.symbol,
-                "dayChange": response1.dayChange(), "overallProfit": response1.overallProfit(),
-                "purchasePrice": response1.pprice, "purchaseDate": response1.pdate,
-                "currentPrice": response1.cprice,
-                "dayVolume": response1.dvolume
+                    for (var index = 0; index < $scope.stockArr.length; index++) {
+                        if ($scope.stockArr[index]["Symbol"] === response1.symbol) {
+                            $scope.stockArr[index]["currentPrice"] = response1.cprice;
+                            $scope.stockArr[index]["dayVolume"] = response1.dvolume;
+                            $scope.stockArr[index]["dayChange"] = response1.dayChange();
+                            $scope.stockArr[index]["overallProfit"] = response1.overallProfit();
+                            break;
+                        }
+                    }
+                }, function (err) {
+                    console.log(err);
+                });
+        }, function (err) {
+            console.log(err);
+        });
+    }
+
+    $scope.searchStock = function (searchStr) {
+        if (searchStr.length > 1) {
+            dataSrv.searchStock(searchStr).then(function (response) {
+                $scope.stockList = response;
+            }, function (err) {
+                console.log(err);
+                $scope.stockList = "";
+            })
+        }
+    }
+
+    $scope.addStockToPortfolio = function (stockName, stockSymbol) {
+
+        dataSrv.getStockInfo(stockName, stockSymbol).then(function (response) {
+            portfolioSrv.addStockToPortfolio(stockName, stockSymbol, response).then(function (response1) {
+                $scope.stockArr.push({
+                    "Name": response1.name, "Symbol": response1.symbol,
+                    "dayChange": response1.dayChange(), "overallProfit": response1.overallProfit(),
+                    "purchasePrice": response1.pprice, "purchaseDate": response1.pdate,
+                    "currentPrice": response1.cprice,
+                    "dayVolume": response1.dvolume
+                });
+            }, function (err) {
+                console.log(err);
             });
         }, function (err) {
             console.log(err);
         });
-    }, function (err) {
-        console.log(err);
-    });
 
-    $scope.stockList = "";
-}
+        $scope.stockList = "";
+    }
 
-$scope.removeStock = function (stock) {
-    var index = $scope.stockArr.indexOf(stock);
-    $scope.stockArr.splice(index, 1);
-}
+    $scope.removeStock = function (stock) {
+        var index = $scope.stockArr.indexOf(stock);
+        $scope.stockArr.splice(index, 1);
+    }
 });
--- app/portfolio/portfolioSrv.js
+++ app/portfolio/portfolioSrv.js
@@ -1,28 +1,31 @@
 app.factory('portfolioSrv', function ($http, $q) {
 
-    function Stock (name, symbol, purchasePrice, purchaseDate, CurrentPrice, dayVolume, dayOpen){
+    function Stock(name, symbol, purchasePrice, purchaseDate, CurrentPrice, dayVolume, dayOpen) {
         this.name = name;
-        this.symbol = symbol; 
+        this.symbol = symbol;
         this.pprice = purchasePrice;
         this.pdate = purchaseDate;
         this.cprice = CurrentPrice;
         this.dvolume = dayVolume;
         this.dopen = dayOpen;
-        this.dayChange = function(){
-            var num = (((this.cprice-this.dopen)/this.dopen)*100);
+        this.dayChange = function () {
+            var num = (((this.cprice - this.dopen) / this.dopen) * 100);
             return num.toFixed(2);
         }
-        this.overallProfit= function(){
-           var num = (((this.cprice-this.pprice)/this.pprice)*100);
-           return num.toFixed(2);
+        this.overallProfit = function () {
+            var num = (((this.cprice - this.pprice) / this.pprice) * 100);
+            return num.toFixed(2);
         }
     }
-    
+
+    var stockArr = [];
+
     function buildStockPortfolio(obj, infoObj) {
         var async = $q.defer();
 
         var stock = new Stock(obj["Name"], obj["Symbol"], obj["purchasePrice"], obj["purchaseDate"],
             infoObj["currentPrice"], infoObj["dayVolume"], infoObj["openPrice"]);
+        stockArr.push(stock);
         //TODO : post stock to user portfolio in DB
         async.resolve(stock);
 
@@ -34,27 +37,49 @@ app.factory('portfolioSrv', function ($http, $q) {
             month = '' + (d.getMonth() + 1),
             day = '' + d.getDate(),
             year = d.getFullYear();
-    
+
         if (month.length < 2) month = '0' + month;
         if (day.length < 2) day = '0' + day;
-    
+
         return "" + year + "-" + month + "-" + day;
     }
 
-    function addStockToPortfolio (stockName, stockSymbol, infoObj) {
+    function addStockToPortfolio(stockName, stockSymbol, infoObj) {
 
         var async = $q.defer();
 
         var stock = new Stock(stockName, stockSymbol, infoObj["currentPrice"], calcCurrentDate(), infoObj["currentPrice"],
-                    infoObj["dayVolume"], infoObj["openPrice"]);
+            infoObj["dayVolume"], infoObj["openPrice"]);
+        stockArr.push(stock);
         //TODO : post stock to user portfolio in DB
         async.resolve(stock);
 
         return async.promise;
     }
 
+    function updateStockInPortfolio(stockName, stockSymbol, infoObj) {
+        var async = $q.defer();
+        var i=0;
+        for (; i < stockArr.length; i++) {
+            if (stockArr[i].symbol === stockSymbol) {
+                stockArr[i].cprice = infoObj["currentPrice"];
+                stockArr[i].dvolume = infoObj["dayVolume"];
+                stockArr[i].dayOpen = infoObj["openPrice"];
+                break;
+            }
+        }
+        //TODO : post stock to user portfolio in DB
+        if (i<stockArr.length)
+            async.resolve(stockArr[i]);
+        else
+            async.reject("stock not found in array");
+
+        return async.promise;
+
+    }
     return {
-        buildStockPortfolio : buildStockPortfolio,
-        addStockToPortfolio : addStockToPortfolio
+        updateStockInPortfolio : updateStockInPortfolio,
+        buildStockPortfolio: buildStockPortfolio,
+        addStockToPortfolio: addStockToPortfolio
     }
 })
